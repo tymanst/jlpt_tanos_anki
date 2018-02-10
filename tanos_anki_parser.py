132463f6290f285d674e8ed3349ce7c5cd398271
Tyman S <tymanst17@gmail.com>
2018-02-10 19:45:33 +0300

    add VocabLists docx, fix KanjiList.N4

--- tanos_anki_parser.py
+++ tanos_anki_parser.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import unicodedata
+from googletrans import Translator
 try:
     from xml.etree.cElementTree import XML
 except ImportError:
@@ -39,36 +40,48 @@ def get_docx_text(path):
 
     return '\n\n'.join(paragraphs)
 
-array = get_docx_text('docx/' + FILE_NAME + '.docx').split('\n')
-array = [i for i in array if i]
+def group(file_name):
+    array = get_docx_text('docx/' + file_name + '.docx').split('\n')
+    array = [i for i in array if i]
 
-kandji = re.compile(u'[\u4E00-\u9FFF]+')
-hiragana = re.compile(u'[\u3040-\u309Fー]+')
-katakana = re.compile(u'[\u30A0-\u30FF]+')
-result = []
-buffer = {}
-for el in array:
-    type = 'eng'
-    type = 'katakana' if katakana.match(el) else type
-    type = 'hiragana' if hiragana.match(el) else type
-    type = 'kandji' if kandji.match(el) else type
-    if type == 'kandji':
-        if 'kandji' in buffer:
-            result.append(buffer)
-        buffer = {'kandji': el}
-    else:
-        buffer[type] = el
+    kandji = re.compile(u'[\u4E00-\u9FFF]+')
+    hiragana = re.compile(u'[\u3040-\u309Fー]+')
+    katakana = re.compile(u'[\u30A0-\u30FF]+')
+    result = []
+    buffer = {}
+    for el in array:
+        type = 'eng'
+        type = 'on' if katakana.match(el) else type
+        type = 'kun' if hiragana.match(el) else type
+        type = 'kanji' if kandji.match(el) else type
+        if type == 'kanji':
+            if 'kanji' in buffer:
+                result.append(buffer)
+            buffer = {'kanji': el}
+        else:
+            buffer[type] = el
+    return result
 
 
-for i, el in enumerate(result):
-    a = os.popen(YARXI_PL + el['kandji']).readline()
-    if a:
-        a = a[:a.find(']')]
-        a = a[a.rfind('[') + 1:]
-        result[i]['russian'] = a
 
-with open('json/' + FILE_NAME + '.json', 'w+') as outfile:
-    json.dump(result, outfile)
-print('done')
+def vocabList(data):
+    for el in data:
+        translator = Translator()
+        el['rus'] = translator.translate(el['kanji'], dst="ru", src='jp')
+    return data
 
-print('done ' + FILE_NAME + ' count:' + str(len(result)))
+
+print(group('VocabList.N4'))
+
+    # for i, el in enumerate(result):
+    #     a = os.popen(YARXI_PL + el['kandji']).readline()
+    #     if a:
+    #         a = a[:a.find(']')]
+    #         a = a[a.rfind('[') + 1:]
+    #         result[i]['russian'] = a
+    #
+    # with open('json/' + FILE_NAME + '.json', 'w+') as outfile:
+    #     json.dump(result, outfile)
+#
+#
+# print('done ' + FILE_NAME + ' count:' + str(len(result)))
